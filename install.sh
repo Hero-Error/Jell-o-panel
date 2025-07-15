d69dd21e3a21ef80c74afaf585446a5aac830f79
Error-Islam <150291023+Hero-Error@users.noreply.github.com>
2025-07-15 22:32:22 +0600

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-apt update -y && apt install -y curl
-curl -s https://raw.githubusercontent.com/JishnuTheGamer/puffer-panel/main/install.sh | bash
-pufferpanel user add --username Toyaha --email heroalfi@gmail.com --password Error-0.1 --admin
-pufferpanel run
