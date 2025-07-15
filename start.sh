24470dfd1540d9c02110af07dc372cf585a16289
Error-Islam <150291023+Hero-Error@users.noreply.github.com>
2025-07-15 16:52:26 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,26 +1,24 @@
 #!/bin/bash
 
 # Set non-interactive for timezone
-export DEBIAN_FRONTEND=noninteractiveIn -fs/usr/share/zoneinfo/Asia/Kathmandu/etc/localtime
-dpkg-reconfigure -f noninteractive tzdata
+export DEBIAN_FRONTEND=noninteractive
 
-#Install tmate and expect
+# Install tmate and expect
 apt-get update
 apt-get install -y tmate expect
 
-#Start tmate in background 
-tmate -S /tmp/tmate.sock new-session-d 
+# Start tmate in background
+tmate -S /tmp/tmate.sock new-session -d
 tmate -S /tmp/tmate.sock wait tmate-ready
 
-#Print SSH and Web (read-write) session links
+# Print SSH and Web (read-write) session links
 echo "SSH access:"
-Comuur changin
 tmate -S /tmp/tmate.sock display -p '#{tmate_ssh}'
 echo "Web access (read-write):"
 tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
 
 # Auto type to keep it alive
 while true; do
-   tmate -S /tmp/tmate.sock send-keys "echo alive && date" C-m
-   sleep 300
+    tmate -S /tmp/tmate.sock send-keys "echo alive && date" C-m
+    sleep 60
 done
